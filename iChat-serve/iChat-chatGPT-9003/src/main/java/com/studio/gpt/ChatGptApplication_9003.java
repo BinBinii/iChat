d8077617aa3df61bf1937f0e169cc28e9675a4da
BinBini <847024724@qq.com>
2023-03-31 14:16:19 +0800

    Update chatgpt sdk

--- iChat-serve/iChat-chatGPT-9003/src/main/java/com/studio/gpt/ChatGptApplication_9003.java
+++ iChat-serve/iChat-chatGPT-9003/src/main/java/com/studio/gpt/ChatGptApplication_9003.java
@@ -1,16 +1,33 @@
 package com.studio.gpt;
 
+import com.unfbx.chatgpt.OpenAiStreamClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
 
 /**
  * @Author: BinBin
  * @Date: 2023/03/31/09:23
  * @Description:
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ChatGptApplication_9003 {
+
+    @Value("${open.ai.token}")
+    private String apiKey;
+    @Value("${open.ai.url}")
+    private String apiHost;
+
     public static void main(String[] args) {
         SpringApplication.run(ChatGptApplication_9003.class, args);
+    }
+
+    @Bean
+    public OpenAiStreamClient openAiStreamClient() {
+        return OpenAiStreamClient.builder().apiHost(apiHost).apiKey(apiKey).build();
     }
 }
